224209ca5da086eca53d84ed296f642233cb75dd
massoud shabani <techdeveloping@outlook.com>
2026-09-01 01:27:19 +0330

    Updates the `AppModule` example to include newline characters in custom log prints, improving the visual separation of log outputs in the console.

--- Examples/basic_app_consumer/app/app_consumer/module.py
+++ Examples/basic_app_consumer/app/app_consumer/module.py
@@ -29,7 +29,7 @@ class AppModule(IModule):
         
 
         if logger and colors:
-            logger.print("This is custom logs: ", tag="app_consumer")
+            logger.print("\nThis is custom logs: ", tag="app_consumer")
             logger.log("AppModule started successfully and using System Logger!", 
                        level="INFO", 
                        tag="app_consumer", 
@@ -48,10 +48,10 @@ class AppModule(IModule):
                        text_color=colors.BRIGHT_MAGENTA,
                        bold=True,
                        italic=True)
-            logger.print("This is a custom test for printing.", color="green", bold=True, tag="app_consumer")
             logger.log("error test", level="ERROR", tag="app_consumer")
             logger.log("warning test", level="WARNING", tag="app_consumer")
             logger.log("critical test", level="CRITICAL", tag="app_consumer")
+            logger.print("End of custom logs.\n", color="green", bold=True, tag="app_consumer")
         else:
             print("   [AppModule] Fallback to standard print because system logger is missing.")
 
